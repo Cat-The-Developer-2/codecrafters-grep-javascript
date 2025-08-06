1c7d8355eeeb8b686f5e167dc7c580983156ff84
unknown <adityachaurasia06@outlook.com>
2025-08-06 16:03:22 +0530

    #sh9

--- app/main.js
+++ app/main.js
@@ -1,35 +1,5 @@
 function matchPattern(inputLine, pattern) {
-  if (pattern === "\\d") {
-    return matchDigit(inputLine);
-  }
-
-  if (pattern === "\\w") {
-    return matchAlphanumeric(inputLine);
-  }
-
-  if (pattern.startsWith("[") && pattern.endsWith("]")) {
-    return positiveCharacter(inputLine, pattern);
-  }
-
-  if (pattern.length === 1) {
-    return inputLine.includes(pattern);
-  } else {
-    throw new Error(`Unhandled pattern ${pattern}`);
-  }
-}
-
-function matchDigit(inputLine) {
-  const regex = /\d/;
-  return regex.test(inputLine);
-}
-
-function matchAlphanumeric(inputLine) {
-  const regex = /[a-zA-Z0-9_]/;
-  return regex.test(inputLine);
-}
-
-function positiveCharacter(inputLine, pattern) {
-  const regex = new RegExp(`[${pattern.slice(1, -1)}]`);
+  const regex = new RegExp(pattern);
 
   return regex.test(inputLine);
 }
