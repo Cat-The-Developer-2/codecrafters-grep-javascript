2c4c13ba3f5fe7b80781891fbdca9601bad6f0b3
unknown <adityachaurasia06@outlook.com>
2025-08-06 16:28:04 +0530

    #sh9_6

--- app/main.js
+++ app/main.js
@@ -1,72 +1,10 @@
 function matchPattern(inputLine, pattern) {
-  const result = [];
-  for (let i = 0; i < pattern.length; i++) {
-    if (pattern[i] === "\\") {
-      if (pattern[i + 1] === "w") {
-        result.push(matchAlphanumeric(inputLine));
-      }
-
-      if (pattern[i + 1] === "d") {
-        result.push(matchDigit(inputLine));
-      }
-    }
-
-    if (pattern[i] === "[") {
-      const Closing = pattern.indexOf("]", i);
-      const subPattern = pattern.slice(i, Closing + 1);
-      result.push(positiveNegativeCharacter(inputLine, subPattern));
-    }
-
-    if (inputLine.includes(pattern)) {
-      result.push(true);
-    }
-
-    if (!inputLine.includes(pattern)) {
-      result.push(false);
-    } else {
-      throw new Error(`Unhandled pattern ${pattern}`);
-    }
+  let regex = pattern.replace("\\d", "[0-9]").replace("\\w", "[0-9a-zA-Z]");
+  if (pattern.length === 1) {
+    return inputLine.includes(pattern);
+  } else {
+    return new RegExp(regex).test(inputLine);
   }
-
-  if (result.includes(false)) {
-    return false;
-  }
-
-  return true;
-
-  // if (pattern === "\\d") {
-  //   return matchDigit(inputLine);
-  // }
-
-  // if (pattern === "\\w") {
-  //   return matchAlphanumeric(inputLine);
-  // }
-
-  // if (pattern.startsWith("[") && pattern.endsWith("]")) {
-  //   return positiveNegativeCharacter(inputLine, pattern);
-  // }
-
-  // if (pattern.length === 1) {
-  //   return inputLine.includes(pattern);
-  // } else {
-  //   throw new Error(`Unhandled pattern ${pattern}`);
-  // }
-}
-
-function matchDigit(inputLine) {
-  const regex = /\d/;
-  return regex.test(inputLine);
-}
-
-function matchAlphanumeric(inputLine) {
-  const regex = /[a-zA-Z0-9_]/;
-  return regex.test(inputLine);
-}
-
-function positiveNegativeCharacter(inputLine, pattern) {
-  const regex = new RegExp(`[${pattern.slice(1, -1)}]`);
-
-  return regex.test(inputLine);
 }
 
 function main() {
